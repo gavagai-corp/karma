7d9ae6dfae9c47874738439b67a76ce19d93104b
fredespi <WPfYi6IQ1oZf>
2017-11-22 16:14:39 +0100

    chore(config): increase timeout
    log the timeout value

--- client/main.js
+++ client/main.js
@@ -11,13 +11,15 @@ var KARMA_URL_ROOT = constants.KARMA_URL_ROOT
 var KARMA_PROXY_PATH = constants.KARMA_PROXY_PATH
 
 // Connect to the server using socket.io http://socket.io
+var timeout = 20000
 var socket = io(location.host, {
   reconnectionDelay: 500,
   reconnectionDelayMax: Infinity,
-  timeout: 2000,
+  timeout: timeout,
   path: KARMA_PROXY_PATH + KARMA_URL_ROOT.substr(1) + 'socket.io',
   'sync disconnect on unload': true
 })
+console.log('timeout: ' + timeout)
 
 // instantiate the updater of the view
 new StatusUpdater(socket, util.elm('title'), util.elm('banner'), util.elm('browsers'))
